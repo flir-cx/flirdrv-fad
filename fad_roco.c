52728ffed9db451451219866c2aab293e43a70b4
Bo Svangård <bo.svangard@flir.se>
2015-03-09 11:04:51 +0100

    Remove functions commented with NOT YET

--- fad_roco.c
+++ fad_roco.c
@@ -215,61 +215,12 @@ BOOL getGPSEnable(BOOL * enabled)
 
 void WdogInit(PFAD_HW_INDEP_INFO pInfo, UINT32 Timeout)
 {
-#ifdef NOT_YET
-	PCSP_WDOG_REGS pWdog;
-	UINT16 wcr;
-
-	if (pInfo->pWdog == NULL) {
-		PHYSICAL_ADDRESS ioPhysicalBase = { CSP_BASE_REG_PA_WDOG1, 0 };
-		pInfo->pWdog =
-		    MmMapIoSpace(ioPhysicalBase, sizeof(PCSP_WDOG_REGS), FALSE);
-	}
-	pWdog = pInfo->pWdog;
-
-	//  WDW = continue timer operation in low-power wait mode
-	//  WOE = tri-state WDOG output pin
-	//  WDA = no software assertion of WDOG output pin
-	//  SRS = no software reset of WDOG
-	//  WRE = generate reset signal upon watchdog timeout
-	//  WDE = disable watchdog (will be enabled after configuration)
-	//  WDBG = suspend timer operation in debug mode
-	//  WDZST = suspend timer operation in low-power stop mode
-	wcr = CSP_BITFVAL(WDOG_WCR_WOE, WDOG_WCR_WOE_TRISTATE) |
-	    CSP_BITFVAL(WDOG_WCR_WDA, WDOG_WCR_WDA_NOEFFECT) |
-	    CSP_BITFVAL(WDOG_WCR_SRS, WDOG_WCR_SRS_NOEFFECT) |
-	    CSP_BITFVAL(WDOG_WCR_WRE, WDOG_WCR_WRE_SIG_RESET) |
-	    CSP_BITFVAL(WDOG_WCR_WDE, WDOG_WCR_WDE_DISABLE) |
-	    CSP_BITFVAL(WDOG_WCR_WDBG, WDOG_WCR_WDBG_SUSPEND) |
-	    CSP_BITFVAL(WDOG_WCR_WDZST, WDOG_WCR_WDZST_SUSPEND) |
-	    CSP_BITFVAL(WDOG_WCR_WT,
-			(UINT16) (Timeout / 500) & WDOG_WCR_WT_MASK);
-
-	// Configure and then enable the watchdog
-	OUTREG16(&pWdog->WCR, wcr);
-	wcr |= CSP_BITFVAL(WDOG_WCR_WDE, WDOG_WCR_WDE_ENABLE);
-	OUTREG16(&pWdog->WCR, wcr);
-
-	WdogService(pInfo);
-#endif
+	pr_info("flirdrv-fad: Watchdog init deprecated\n");
 }
 
 BOOL WdogService(PFAD_HW_INDEP_INFO pInfo)
 {
-#ifdef NOT_YET
-	PCSP_WDOG_REGS pWdog;
-
-	if (pInfo->pWdog == NULL) {
-		return FALSE;
-	}
-	pWdog = pInfo->pWdog;
-
-	// 1. write 0x5555
-	pWdog->WSR = WDOG_WSR_WSR_RELOAD1;
-
-	// 2. write 0xAAAA
-	pWdog->WSR = WDOG_WSR_WSR_RELOAD2;
-
-#endif
+	pr_info("flirdrv-fad: WatchdogService deprecated\n");
 	return TRUE;
 }
 
@@ -284,214 +235,50 @@ BOOL GetLaserActive(PFAD_HW_INDEP_INFO pInfo)
 
 void SetBuzzerFrequency(USHORT usFreq, UCHAR ucPWM)
 {
-#ifdef NOT_YET
-	static PCSP_PWM_REG pPWM;
-	int period;
-	int sample;
-
-	if (pPWM == NULL) {
-		PHYSICAL_ADDRESS phyAddr;
-
-		DDKClockSetGatingMode(DDK_CLOCK_GATE_INDEX_PWM2_IPG,
-				      DDK_CLOCK_GATE_MODE_ENABLED_ALL);
-
-		phyAddr.QuadPart = CSP_BASE_REG_PA_PWM2;
-		pPWM =
-		    (PCSP_PWM_REG) MmMapIoSpace(phyAddr, sizeof(CSP_PWM_REG),
-						FALSE);
-
-		pPWM->PWMCR = 0x00C10416;	// 1 MHz, enabled during debug
-	}
-
-	if (usFreq && ucPWM) {
-		period = 1000000 / usFreq;
-
-		if (period < 10)
-			period = 10;
-		if (period > 0xFFFE)
-			period = 0xFFFE;
-
-		sample = (period * ucPWM + 50) / 100;
-		if (sample == 0)
-			sample = 1;
-
-		pPWM->PWMSAR = sample;
-		pPWM->PWMPR = period;
-		pPWM->PWMCR |= 1;
-	} else {
-		pPWM->PWMCR &= ~1;
-	}
-#endif
+	pr_info("flirdrv-fad SetBuzzerFrequency not implemented\n");
 }
 
 DWORD SetKeypadSubjBacklight(PFAD_HW_INDEP_INFO pInfo,
 			     PFADDEVIOCTLSUBJBACKLIGHT pBacklight)
 {
+	pr_info("flirdrv-fad SetKeypadSubjBacklight not implemented\n");
 	return 0;
 }
 
-//------------------------------------------------------------------------------
-//
-//  Function:  GetKeypadSubjBacklight
-//
-//  This method returns the keypad backlight in subjective levels.
-//      Since this function works in parallel with the older functions setting
-//      percentage values, it copes with values that differs from the defined 
-//  subjective levels.
+
+/** 
+ * 
+ *  Function:  GetKeypadSubjBacklight
+ *
+ *    This method returns the keypad backlight in subjective levels.
+ *    Since this function works in parallel with the older functions setting
+ *    percentage values, it copes with values that differs from the defined 
+ *    subjective levels.
+ *
+ * @param pInfo 
+ * @param pBacklight 
+ * 
+ * @return 
+ */
 DWORD GetKeypadSubjBacklight(PFAD_HW_INDEP_INFO pInfo,
 			     PFADDEVIOCTLSUBJBACKLIGHT pBacklight)
 {
+	pr_info("flirdrv-fad GetKeypadSubjBackligt not implemented\n");
 	return ERROR_SUCCESS;
 }
 
-#ifdef NOT_YET
-DWORD getLedState(PFAD_HW_INDEP_INFO pInfo, FADDEVIOCTLLED * pLedData)
-{
-	DWORD pinState;
-
-	DDKGpioReadDataPin(FLIR_GPIO_PIN_POWER_ON_LED, &pinState);
-
-	if (pInfo->dwLedFlash) {
-		pLedData->eColor = LED_COLOR_GREEN;
-		if (pInfo->dwLedFlash < 500)
-			pLedData->eState = LED_FLASH_FAST;
-		else
-			pLedData->eState = LED_FLASH_SLOW;
-	} else if (pinState) {
-		pLedData->eColor = LED_COLOR_GREEN;
-		pLedData->eState = LED_STATE_ON;
-	} else {
-		pLedData->eColor = LED_COLOR_OFF;
-		pLedData->eState = LED_STATE_OFF;
-	}
-	return ERROR_SUCCESS;
-}
-
-DWORD setLedState(PFAD_HW_INDEP_INFO pInfo, FADDEVIOCTLLED * pLedData)
-{
-	DWORD dwErr = ERROR_SUCCESS;
-
-	DDKIomuxSetPinMux(FLIR_IOMUX_PIN_POWER_ON_LED);
-	DDKIomuxSetPadConfig(FLIR_IOMUX_PAD_POWER_ON_LED);
-	DDKGpioSetConfig(FLIR_GPIO_PIN_POWER_ON_LED, DDK_GPIO_DIR_OUT,
-			 DDK_GPIO_INTR_NONE);
-
-	if ((pLedData->eColor != LED_COLOR_OFF) &&
-	    (pLedData->eState == LED_STATE_ON)) {
-		DDKGpioWriteDataPin(FLIR_GPIO_PIN_POWER_ON_LED, 1);
-		pInfo->dwLedFlash = 0;
-	} else if ((pLedData->eColor == LED_COLOR_OFF) ||
-		   (pLedData->eState == LED_STATE_OFF)) {
-		DDKGpioWriteDataPin(FLIR_GPIO_PIN_POWER_ON_LED, 0);
-		pInfo->dwLedFlash = 0;
-	} else if (pLedData->eState == LED_FLASH_SLOW) {
-		pInfo->dwLedFlash = 1000;
-		SetEvent(pInfo->hLedFlashEvent);
-	} else if (pLedData->eState == LED_FLASH_FAST) {
-		pInfo->dwLedFlash = 100;
-		SetEvent(pInfo->hLedFlashEvent);
-	} else {
-		dwErr = ERROR_INVALID_PARAMETER;
-		pInfo->dwLedFlash = 0;
-	}
-	return dwErr;
-}
-
-DWORD WINAPI fadFlashLed(PVOID pContext)
-{
-	PFAD_HW_INDEP_INFO pInfo = (PFAD_HW_INDEP_INFO) pContext;
-	ULONG timeout;
-	BOOL state = FALSE;
-
-	CeSetThreadPriority(GetCurrentThread(), 246);
-
-	while (TRUE) {
-		// Wait for ISR interrupt notification
-		if (pInfo->dwLedFlash == 0)
-			timeout = INFINITE;
-		else
-			timeout = pInfo->dwLedFlash / 2;
-		WaitForSingleObject(pInfo->hLedFlashEvent, timeout);
-
-		if (pInfo->dwLedFlash) {
-			state = !state;
-			DDKGpioWriteDataPin(FLIR_GPIO_PIN_POWER_ON_LED, state);
-		}
-	}
-	return (0);
-}
-#endif
 
 DWORD GetKeypadBacklight(PFADDEVIOCTLBACKLIGHT pBacklight)
 {
-#ifdef NOT_YET
-	UINT8 current;
-
-	PmicBacklightGetCurrentLevel(BACKLIGHT_KEYPAD, &current);
-
-	pBacklight->backlight = current * 25;
-#endif
+	pr_info("GetKeypadBackligt not implemented\n");
 	return ERROR_SUCCESS;
 }
 
 DWORD SetKeypadBacklight(PFADDEVIOCTLBACKLIGHT pBacklight)
 {
-#ifdef NOT_YET
-	UINT8 level;
-	UINT8 backlight = pBacklight->backlight;
-
-	if ((backlight > 100) || (backlight < 0))
-		return ERROR_INVALID_PARAMETER;
-
-	//turn the 0-100 range to mc13892's current range 0-4;
-	level = (backlight + 12) / 25;
-
-	// set the current level for max for the Display
-	PmicBacklightSetCurrentLevel(BACKLIGHT_KEYPAD, level);
-	PmicBacklightSetDutyCycle(BACKLIGHT_KEYPAD, 32);
-	PmicBacklightSetCurrentLevel(BACKLIGHT_AUX_DISPLAY, level);
-	PmicBacklightSetDutyCycle(BACKLIGHT_AUX_DISPLAY, 32);
-#endif
+	pr_info("SetKeypadBacklight not implemented\n");
 	return ERROR_SUCCESS;
 }
-
-#ifdef NOT_YET
-void BspFadPowerDown(BOOL down)
-{
-	static FADDEVIOCTLBACKLIGHT backlight;
-
-	DDKIomuxSetPinMux(FLIR_IOMUX_PIN_POWER_ON_LED);
-	DDKIomuxSetPadConfig(FLIR_IOMUX_PAD_POWER_ON_LED);
-	DDKGpioSetConfig(FLIR_GPIO_PIN_POWER_ON_LED, DDK_GPIO_DIR_OUT,
-			 DDK_GPIO_INTR_NONE);
-	DDKGpioWriteDataPin(FLIR_GPIO_PIN_POWER_ON_LED, (down == FALSE));
-
-	// Keypad backlight handled here as it must be used on ioctl level to use CSPI
-	if (down) {
-		// Keypad backlight
-		GetKeypadBacklight(&backlight);
-		PmicBacklightSetCurrentLevel(BACKLIGHT_KEYPAD, 0);
-		PmicBacklightSetCurrentLevel(BACKLIGHT_AUX_DISPLAY, 0);
-
-		// PIRI I2C expander
-		SetI2CIoport(VCM_LED_EN, FALSE);
-		SetI2CIoport(FOCUS_POWER_EN, FALSE);
-
-		// 5V and 3V3 for USB PHY
-		PmicRegisterWrite(MC13892_CHG_USB1_ADDR, 1, 0x409);
-	} else {
-		// Keypad backlight
-		SetKeypadBacklight(&backlight);
-
-		// PIRI I2C expander
-		SetI2CIoport(VCM_LED_EN, TRUE);
-		SetI2CIoport(FOCUS_POWER_EN, TRUE);
-
-		// 5V and 3V3 for USB PHY
-		PmicRegisterWrite(MC13892_CHG_USB1_ADDR, 0x409, 0x409);
-	}
-}
-#endif
 
 void BspGetSubjBackLightLevel(UINT8 * pLow, UINT8 * pMedium, UINT8 * pHigh)
 {
