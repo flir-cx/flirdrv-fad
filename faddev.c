flir-cx/flirdrv-fad

commit f31f9d1f5905e93fc344198cb3f72a11de05c1c5

Modify wakup sources...

faddev.c
@@ -55,7 +55,7 @@ static long FAD_IOControl(struct file *filep,
 static unsigned int FadPoll(struct file *filp, poll_table * pt);
 static ssize_t FadRead(struct file *filp, char __user * buf, size_t count,
 		       loff_t * f_pos);
-struct wakeup_source *get_suspend_wakup_source(void);
+/* struct wakeup_source *get_suspend_wakup_source(void); */
 static PFAD_HW_INDEP_INFO gpDev;
 
 static struct file_operations fad_fops = {
@@ -71,6 +71,19 @@ static struct miscdevice fad_miscdev = {
 	.name  = "fad0",
 	.fops  = &fad_fops
 };
+
+
+#if KERNEL_VERSION(4,0,0) > LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0)
+//get_suspend_wakup_source implemented in evander/lennox kernel
+struct wakeup_source *get_suspend_wakup_source(void);
+#else
+void *get_suspend_wakup_source(void)
+{
+	return NULL;
+}
+#endif
+
+
 
 // Code
 
@@ -263,16 +276,16 @@ static struct attribute_group faddev_sysfs_attr_grp = {
  */
 int get_wake_reason(void)
 {
-	struct wakeup_source *ws = NULL;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0)
+	struct wakeup_source *ws;
 	ws = get_suspend_wakup_source();
-#endif
 
 	if(timed_standby)
 		return ON_OFF_BUTTON_WAKE;
 
-	if(!ws)
-		goto err_wake;
+	if(!ws) {
+		pr_err("fad: No suspend wakeup source");
+		return UNKNOWN_WAKE;
+	}
 
 	if(strstr(ws->name,"onkey"))
 		return ON_OFF_BUTTON_WAKE;
@@ -281,12 +294,11 @@ int get_wake_reason(void)
 		return USB_CABLE_WAKE;
 
 	if(strstr(ws->name,"rtc") && !timed_standby) {
-		pr_err("Standby shutdown\n");
+		pr_err("fad: Standby shutdown\n");
 		orderly_poweroff(1);
 		return UNKNOWN_WAKE;
 	}
 
-err_wake:
 	pr_err("Unknown suspend wake reason");
 	return UNKNOWN_WAKE;
 }

commit ee7a0f17a6286df6e54601aeaa4d2b4c845dee04

Corrected selection on wakeup check dependent on kernel version [CIRRUS-473]
(The replaced #if ... did not work
 - standby->off broken since f31f9d1 - 2019)
Kernel method *get_suspend_wakup_source() exists in FLIR evco (3.14)
kernel tree. A working method is essential for system functionality
standby->off (after 6 hours standby)
The method does not exist / is unneeded in neco (3.0), roco (3.10)
These targets do not support standby

fad is not used in ec201 ("Sherlock") - 4.x kernel
standby->off is then handled "elsewhere"

For "cirrus" (5.10 kernel) the method does not exist in kernel tree
A working solution for cirrus/5.10 remains to be implemented...

faddev.c
@@ -84,10 +84,13 @@ static struct miscdevice fad_miscdev = {
 };
 
 
-#if KERNEL_VERSION(4,0,0) > LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0)
-//get_suspend_wakup_source implemented in evander/lennox kernel
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0) && LINUX_VERSION_CODE <  KERNEL_VERSION(3,15,0)
+// get_suspend_wakup_source is implemented in evander/lennox (3.14.x) kernel
 struct wakeup_source *get_suspend_wakup_source(void);
+#warning "we use (3.14.x) kernel get_suspend_wakeup_source"
+
 #else
+#warning "local dummy wakeup compatibility due to kernel version check - do not expect 6 hour standby->off to work"
 void *get_suspend_wakup_source(void)
 {
 	return NULL;